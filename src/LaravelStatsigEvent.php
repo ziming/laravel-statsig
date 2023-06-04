51ec54a828f3351505eccaf83d2bce0e2c4197d1
ziming <ziming@users.noreply.github.com>
2023-06-04 10:12:29 +0000

    Fix styling

--- src/LaravelStatsigEvent.php
+++ src/LaravelStatsigEvent.php
@@ -2,16 +2,8 @@
 
 namespace Ziming\LaravelStatsig;
 
-use Closure;
 use Illuminate\Foundation\Auth\User;
-use InvalidArgumentException;
-use Statsig\Adapters\IDataAdapter;
-use Statsig\Adapters\ILoggingAdapter;
-use Statsig\DynamicConfig;
-use Statsig\Layer;
 use Statsig\StatsigEvent;
-use Statsig\StatsigOptions;
-use Statsig\StatsigServer;
 use Statsig\StatsigUser;
 use Ziming\LaravelStatsig\Utils\LaravelUserToStatsigUserConverter;
 
