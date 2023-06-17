8c7ba9dc7f72c61b23204b1923106ca9365da222
ziming <ziming.opensource@gmail.com>
2023-06-17 11:23:14 +0800

    output command result

--- src/Commands/StatsigSendCommand.php
+++ src/Commands/StatsigSendCommand.php
@@ -30,6 +30,7 @@ class StatsigSendCommand extends Command
         }
 
         $result = Process::run($commandToRun);
+        $this->info($result->output());
 
         return $result->exitCode();
     }
--- src/Commands/StatsigSyncCommand.php
+++ src/Commands/StatsigSyncCommand.php
@@ -30,6 +30,7 @@ class StatsigSyncCommand extends Command
         }
 
         $result = Process::run($commandToRun);
+        $this->info($result->output());
 
         return $result->exitCode();
     }
